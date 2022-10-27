b2d50c23cb4f55362e8b03273a8371c9c5d75ec3
jj <lucax.oliveira96@gmail.com>
2022-10-27 16:11:42 -0300

    more tests

--- cypress/e2e/ui/devbook.cy.js
+++ cypress/e2e/ui/devbook.cy.js
@@ -3,4 +3,13 @@ describe('devbook application', () => {
         cy.visit('localhost:3000/')
         cy.get('h2[data-test="heading"]').contains('DevBook!')
     })
+    it('Shows a book list', () =>{
+        cy.visit('localhost:3000/')
+        cy.get('div[data-test="book-list"]').should('exist')
+        cy.get('div.book-item').should((books) => {
+            expect(books).to.have.length(2);
+            const titles = [ ...books].map(x => x.querySelector('h2').innerHTML)
+            expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
+        })
+    })
 })
--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <>
       <Typography variant="h2" component="h2" data-test="heading">DevBook!</Typography>
+      <div data-test="book-list">
+        <div className="book-item">
+          <h2 className="titles">Refactoring</h2>
+        </div>
+        <div className="book-item">
+          <h2 className="titles">Domain-driven design</h2>
+        </div>
+      </div>
     </>
   );
 }
